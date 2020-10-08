4dda64c0347467681830e20a88ba5244eec3351d
elzup <guild0105@gmail.com>
2020-10-08 21:52:15 +0900

    add cell.char

--- src/components/Scan.tsx
+++ src/components/Scan.tsx
@@ -7,6 +7,7 @@ const { useEffect, useState } = React
 
 type Cell = {
   state: number
+  chara: string
   color: string
 }
 
@@ -16,6 +17,7 @@ function useCell() {
     const t = range(7).map(() =>
       range(7).map(() => ({
         state: Math.floor(Math.random() * 5),
+        chara: '#',
         color: convert.hsl.hex([Math.floor(Math.random() * 360), 50, 50]),
       }))
     )
@@ -32,9 +34,9 @@ const Scan = () => {
     <Box flexDirection="column">
       {cells.map((row, ri) => (
         <Box key={ri}>
-          {row.map((area, ai) => (
-            <Text key={ai} color={area.color}>
-              {' *'}
+          {row.map((cell, ai) => (
+            <Text key={ai} color={cell.color}>
+              {cell.chara}
             </Text>
           ))}
         </Box>
