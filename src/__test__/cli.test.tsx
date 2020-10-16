61bc98b90bfba43f1f1ea364f2d14b58c0994fa5
elzup <guild0105@gmail.com>
2020-10-16 20:42:53 +0900

    make render snapshot

--- src/__test__/cli.test.tsx
+++ src/__test__/cli.test.tsx
@@ -1,20 +1,22 @@
 import * as React from 'react'
 import * as chalk from 'chalk'
 import { render } from 'ink-testing-library'
+import { advanceBy, advanceTo, clear } from 'jest-date-mock'
 import App from '../App'
 import { readFileSync } from 'fs'
-import { readFile } from 'fs/promises'
 
 jest.mock('../service')
 import { getRandomCodes } from '../service'
 
+// @ts-ignore
+getRandomCodes.mockImplementation(() => new Promise(obj))
+
 const obj = readFileSync(__dirname + '/getRandomCodes.json')
 
-// @ts-ignore
-getRandomCodes.mockImplementation(() => obj)
-
-test('greet unknown user', (t) => {
+test('render', (t) => {
+  advanceTo(new Date(2020, 10, 10, 0, 0, 0))
   const { lastFrame } = render(<App />)
 
-  expect(lastFrame()).toBe(chalk`Hello, {green Stranger}`)
+  expect(lastFrame()).toMatchSnapshot('first')
+  clear()
 })
